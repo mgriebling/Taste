59d7995b003f1fe44e9ffbc0c07077791db94759
Michael Griebling <michael.griebling@solinst.com>
2017-08-08 08:15:14 -0400

    Initial Commit

---
+++ Taste/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Taste
+//
+//  Created by Michael Griebling on 8Aug2017.
+//  Copyright © 2017 Solinst Canada. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
